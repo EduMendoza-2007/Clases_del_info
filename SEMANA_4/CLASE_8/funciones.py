a3c8c806f0f87c51c2635dde13d2ea58b27a8ba9
EduMendoza-2007 <mendozaeduardoomar@gmail.com>
2025-06-03 09:44:26 -0300

    funciones y sus tipos

--- SEMANA_4/CLASE_8/funciones.py
+++ SEMANA_4/CLASE_8/funciones.py
@@ -19,8 +19,8 @@
 # print(f"Cantidad de notas: {len(notas)}")
 # print(f"Promedio: {promedio}")
 
-def promedio(notas):
-    return sum(notas) / len(notas)
+# def promedio(notas):
+#     return sum(notas) / len(notas)
 
 # promedio_Edu = promedio([5, 6, 8, 9])
 # promedio_Juan = promedio([])
@@ -71,11 +71,55 @@ def promedio(notas):
 # }
 # ]
 
-variable_global = "global"
+# variable_global = "global"
 
-def prueba():
-    variable_local = "local"
-    print(variable_local)
+# def prueba():
+#     variable_local = "local"
+#     print(variable_local)
 
-prueba()
-print(variable_global)
+# prueba()
+# print(
+
+#*args: Permiten pasar a una funcion un numero variable/ indeterminado de argumentos posicionales...
+
+#ALta calculadora hermano
+
+# def suma(*numeros):
+#     """Calculadora solamente para suma"""
+#     print(numeros)
+#     print(type(numeros))
+
+#     resultado = 0
+#     for numero in numeros:
+#         resultado += numero
+
+#     return resultado
+
+# resultado = suma(10, 10, 10, 20, 30)
+# print(f"Resultado es: {resultado})
+
+
+#*kwargs:Permiten pasar a una funcion un numero de variables/ indeterminado de argumentos con nombre(clave)-valor)
+
+
+# def ver_persona(**kwargs):
+#     print(kwargs)
+#     print(type(kwargs))
+
+#     for clave, valor in kwargs.items():
+#         print(f"Clave: {clave}, Valor: {valor}")
+
+# ver_persona(nombre = "Juan", edad = 25, ciudad = "Resistencia")
+
+#Respetar el orden siempre
+         #indeterminados o determinados
+                 #posicionales
+def mostrar_info(*args, nombre, edad=0, **kwargs):
+                                    #nombrados
+                         #determinados o indeterminados
+    print(f"Posicionales indeterminados {args}")
+    print(f"Nombrados indeterminados: {kwargs}")
+    print(f"posicionales determinados: {nombre}")
+    print(f"Nombrados determinados: {edad}")
+
+mostrar_info(50, True, "algo", nombre="Pablo", edad=20, ciudad="Resistencia")
